a853d7d9e4ee748c52c90bc60846590981603410
MrWeber94 <abdelkareemelsayed2014@gmail.com>
2021-10-24 15:44:34 +0200

    v10.24

---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+
+Route::get('Message/{id}/{name?}',function ($id,$name = "Vistor"){
+    echo 'welcome  '.$name.' Your Id :'.$id ;
+})->where(['id' => '[0-9]+', 'name'=>'[a-z]+']);
+
+
+// Route::get('Create',function (){
+//     return  view('register');
+// });
+
+
+// Route::view('Create', 'register');
+
+
+
+
+/*
+get 
+post 
+put 
+patch 
+delete
+
+
+=========
+view
+*/
